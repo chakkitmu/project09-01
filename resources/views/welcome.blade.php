ca72f5560bb6a09b1a64af0ad81b3c3f6add507f
chakkitmu <chakkitmu1550@gmail.com>
2023-10-30 13:27:38 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="viewport" content="width=device-widti">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h1>จักรกฤต ม่วงเถื่อน</h1> <br>
+        <h1>{{ dete("d:m:Y H:i:s") }}</h1>
+    </center>    
+</body>
+</html>
